e62384507eda27163e8feb075281933ae7e052b6
Alexey Zakharov <alexey.a.zakharov@gmail.com>
2023-09-09 13:30:33 +0300

    hh: auto raise up the resume

--- src/index.js
+++ src/index.js
@@ -1,19 +1,21 @@
 import puppeteer from 'puppeteer';
 
-const browser = await puppeteer.launch({headless: false});
+const browser = await puppeteer.launch({
+  headless: false,
+  userDataDir: "./UserData"
+});
 const page = await browser.newPage()
-const navigationPromise = page.waitForNavigation()
-
-await page.goto('https://nn.hh.ru/')
-
+await page.goto('https://nn.hh.ru/', {waitUntil: 'networkidle2'})
 await page.setViewport({ width: 1920, height: 1061 })
 
-await page.waitForSelector('.bloko-columns-wrapper > .bloko-column > .supernova-navi > .supernova-navi-item:nth-child(2) > .supernova-link')
-await page.click('.bloko-columns-wrapper > .bloko-column > .supernova-navi > .supernova-navi-item:nth-child(2) > .supernova-link')
+const selectorMyResumesLink = '.bloko-columns-wrapper > .bloko-column > .supernova-navi > .supernova-navi-item:nth-child(2) > .supernova-link';
+await page.waitForSelector(selectorMyResumesLink)
+await page.click(selectorMyResumesLink)
 
-await navigationPromise
+// Try to find the button "Поднять в поиске"
+const [raiseResumeButton] = await page.$x("//button[contains(., 'Поднять в поиске')]");
+if (raiseResumeButton) {
+  await raiseResumeButton.click();
+}
 
-await page.waitForSelector('.applicant-resumes-recommendations > .applicant-resumes-recommendations-buttons > div > .applicant-resumes-recommendations-button > .bloko-button')
-await page.click('.applicant-resumes-recommendations > .applicant-resumes-recommendations-buttons > div > .applicant-resumes-recommendations-button > .bloko-button')
-
-//await browser.close()
+await browser.close()
